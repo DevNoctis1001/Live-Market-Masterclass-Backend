6110315c233f05408d395a101e4b8af3cada2b86
DevNoctis1001 <yuki65084@gmail.com>
2025-03-26 11:36:19 -0500

    feat: add parse message feature.

--- config/Parse_test.py
+++ config/Parse_test.py
@@ -1,13 +1,9 @@
-import os.path
 import re
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-# If modifying these scopes, delete the file token.json.
 
-# Order class
+
+#94165383605 BOT +18 NVDA 100 (Weeklys) 28 MAR 25 114 PUT @1.23LAST=115.78 BID=115.77 ASK=115.78 MARK=115.78 CLOSE=null , ACCOUNT *****530SCHW,
+#94165383605 SOLD -20 NVDA 100 (Weeklys) 28 MAR 25 114 CALL @1.23LAST=115.78 BID=115.77 ASK=115.78 MARK=115.78 CLOSE=115.78 , ACCOUNT *****SCsSCHW,
+
 class OrderDetail:
     def __init__(self):
         self.OrderId = ""
@@ -44,11 +40,6 @@ class OrderDetail:
         print(f"MarkPrice: {self.MarkPrice}")
         print(f"ClosePrice: {self.ClosePrice}")
         print(f"Account: {self.Account}")
-        print("----------------------------------------\n")
-
-SCOPES = ["https://mail.google.com/"]
-
-
 
 def ParseStringByRe(message) :
     pattern  = r'(#\d+) ([A-Z]+) ([+-]?\d+) ([A-Z]+) (\d+) \((\w+)\) (\d+) (\w+) (\d+) (\d+) (\w+) @(\d+\.\d+)LAST=(\d+\.\d+) BID=(\d+\.\d+) ASK=(\d+\.\d+) MARK=(\d+\.\d+) CLOSE=(\w+) , ACCOUNT (\*{5}\w+)'
@@ -72,51 +63,10 @@ def ParseStringByRe(message) :
         extractedOrder.MarkPrice   =   match.group(16)
         extractedOrder.ClosePrice  =   match.group(17)
         extractedOrder.Account     =   match.group(18)
-        extractedOrder.print()
-    else :
-       print("Don't match")
-  
 
-def main():
-  creds = None
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
+    extractedOrder.print()
 
-  try:
-    # Call the Gmail API
-    service = build("gmail", "v1", credentials=creds)
-    sender_email = "alerts@thinkorswim.com"
-    recipient_email = 'marketmasterclasstosalerts@gmail.com' 
-    query = f"from:{sender_email} to:{recipient_email}"
-    results = service.users().messages().list(userId="me", q=query).execute()
-    messages = results.get("messages", [])
-    if not messages:
-      print("No messages found.")
-      return 
-    for message in messages:
-      msg = service.users().messages().get(userId='me', id=message['id']).execute()
-      headers = msg['payload']['headers']
-      subject = next((header['value'] for header in headers if header['name'] == 'Subject'), None)
-      ParseStringByRe(msg['snippet'])
-      
-      # print(f"Message ID: {message['id']}, Subject: {subject}, Snippet: {msg['snippet']}") 
+if __name__ == "__main__" :
+    message ="#94165383605 BOT +1 NVDA 100 (Weeklys) 28 MAR 25 114 PUT @1.23LAST=115.78 BID=115.77 ASK=115.78 MARK=115.78 CLOSE=null , ACCOUNT *****530SCHW"
 
-  except HttpError as error: 
-    print(f"An error occurred: {error}")
-
-
-if __name__ == "__main__":
-  while True:
-    main()
+    ParseStringByRe(message)
